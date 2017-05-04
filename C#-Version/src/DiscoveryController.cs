1c5c844149854d9b664cc5155a0b401035087037
Phong Trinh <101618268@student.swin.edu.au>
2017-05-04 14:45:35 +1000

    hold c to see enemy's ships

--- C#-Version/src/DiscoveryController.cs
+++ C#-Version/src/DiscoveryController.cs
@@ -31,6 +31,8 @@ static class DiscoveryController
 		}
 	}
 
+
+
 	/// <summary>
 	/// Attack the location that the mouse if over.
 	/// </summary>
@@ -63,7 +65,7 @@ static class DiscoveryController
 		const int HITS_TOP = 206;
 		const int SPLASH_TOP = 256;
 
-		if ((SwinGame.KeyDown(KeyCode.vk_LSHIFT) | SwinGame.KeyDown(KeyCode.vk_RSHIFT)) & SwinGame.KeyDown(KeyCode.vk_c)) {
+		if (SwinGame.KeyDown(KeyCode.vk_c)) {
 			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, true);
 		} else {
 			UtilityFunctions.DrawField(GameController.HumanPlayer.EnemyGrid, GameController.ComputerPlayer, false);
