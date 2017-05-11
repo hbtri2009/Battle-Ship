e97cacd22392df2f7f58eb37165df2697767dceb
TuongNg <100662345@student.swin.edu.au>
2017-05-12 00:17:09 +1000

    Update GameInstructionsController.cs

--- C#-Version/src/GameInstructionsController.cs
+++ C#-Version/src/GameInstructionsController.cs
@@ -12,23 +12,26 @@ static class GameInstructionsController
 	public static void DrawInstructions ()
 	{
 		const int INTRUCTIONS_LEFT = 10;
-		const int INTRUCTIONS_GAP = 100;
-		const int INTRUCTIONS_HEADING = 40;
+		const int INTRUCTIONS_GAP = 20;
+		const int INTRUCTIONS_HEADING = 150;
 
-		SwinGame.DrawText ("   High Scores   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING);
-		SwinGame.DrawText ("   How To Play   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 2 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   1/ Select difficulty   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 3 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   2/ Place all the ships (or you can click the random button to randomly place your ships)", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 5 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   3/ The players take turn to fire   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 6 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("      If you hit the enemy's ship you get one more shot until you miss the shot then the next turn will be the enemy's turn.   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 7 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("      Each ship has different length   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 8 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("      For example if the ship is 4 square long you have to hit all 4 square to sink that ship   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 9 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("      The first one to sink all the enemy ships win   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 10 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   Game controller   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 12 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   M to mute the music   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 14 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   U to unmute the music   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 15 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   UP ARROW or DOWN ARROW to rotate the ship vertically   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 16 * INTRUCTIONS_GAP);
-		SwinGame.DrawText ("   LEFT ARROW OR RIGHT ARROW to rotate the ship horizontally   ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + 17 * INTRUCTIONS_GAP);
+		SwinGame.DrawText ("Instruction", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING);
+		SwinGame.DrawText ("How to play", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 2);
+		SwinGame.DrawText ("1/Select difficulty", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 4);
+		SwinGame.DrawText ("2/Place the ship (or you can click the random button to automatically place your ships randomly", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 5);
+		SwinGame.DrawText ("3/Players take turn to shoot each other ships", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 6);
+		SwinGame.DrawText ("  You get one more shot if you hit the enemy team untill you miss your shot", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 7);
+		SwinGame.DrawText ("  Each ship has different length", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 8);
+		SwinGame.DrawText ("  For example if the ship is 4 square long then you need to hit all 4 square to sink the ship", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 9);
+		SwinGame.DrawText ("  The first one to sink all the enemy's ships win", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 10);
+		SwinGame.DrawText ("Game Controller", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 12);
+		SwinGame.DrawText ("M to mute the music", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 14);
+		SwinGame.DrawText ("U to unmute the music", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 15);
+		SwinGame.DrawText ("UP ARROW or DOWN ARROW to place your ship vertically", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 16);
+		SwinGame.DrawText ("LEFT ARROW or RIGHT ARROW to place your ship horizontally ", Color.White, GameResources.GameFont ("Courier"), INTRUCTIONS_LEFT, INTRUCTIONS_HEADING + INTRUCTIONS_GAP * 17);
+
+
+
 		SwinGame.RefreshScreen ();
 
 	}
